9a616270864865ba5a5f6f1014513e1ab3e6b8bc
Aurie <Myrdden42@gmail.com>
2019-05-01 14:19:01 -0600

    Rename create for post

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/comedians', to: 'comedians#index'
-  get '/comedians/new', to: 'comedians#create'
+  get '/comedians/new', to: 'comedians#new_comedian'
+  post '/comedians/new', to: 'comedians#create'
 end
